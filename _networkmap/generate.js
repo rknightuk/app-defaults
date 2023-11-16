37911f7aa6ac1cb8857b5fdd08ae73a68b795fb4
Robb Knight <desk@rknight.me>
2023-11-16 21:16:40 +0000

    this works

--- _networkmap/generate.js
+++ _networkmap/generate.js
@@ -2,66 +2,130 @@ import { extract } from '@extractus/article-extractor'
 import { DOMParser, parseHTML } from 'linkedom'
 import fs from 'fs'
 
-const sites = JSON.parse(fs.readFileSync('../_data/sites.json'))
-
 const normaliseUrl = (url) => {
     return url.replace(/\/$/, '')
 }
 
-const data = {
-    'https://listen.hemisphericviews.com/097': {
-        id: 1000,
-        urlKey: 'https://listen.hemisphericviews.com/097',
-        url: 'https://listen.hemisphericviews.com/097',
-        links: [],
-        name: 'Hemispheric Views',
-        shape: 'box',
-    },
-    'https://defaults.rknight.me': {
-        id: 1001,
-        urlKey: 'https://defaults.rknight.me',
-        url: 'https://defaults.rknight.me',
-        links: [],
-        name: 'App Defaults',
-        shape: 'box',
-    },
+const sites = JSON.parse(fs.readFileSync('../_data/sites.json'))
+
+const generatePostDataFromSites = () => {
+    const data = {
+        'https://listen.hemisphericviews.com/097': {
+            id: 1000,
+            urlKey: 'https://listen.hemisphericviews.com/097',
+            url: 'https://listen.hemisphericviews.com/097',
+            links: [],
+            name: 'Hemispheric Views',
+            shape: 'box',
+        },
+        'https://defaults.rknight.me': {
+            id: 1001,
+            urlKey: 'https://defaults.rknight.me',
+            url: 'https://defaults.rknight.me',
+            links: [],
+            name: 'App Defaults',
+            shape: 'box',
+        },
+    }
+
+    sites.forEach((site, i) => {
+        const url = normaliseUrl(site.url)
+        data[url] = {
+            ...site,
+            id: i + 1,
+            urlKey: url,
+            links: [],
+            url: url,
+        }
+    })
+
+    return data
 }
 
-sites.forEach((site, i) => {
-    const url = site.url.replace(/\/$/, '')
-    data[url] = {
-        id: i + 1,
-        urlKey: url,
-        links: [],
-        ...site,
-    }
-})
-
 const run = async () => {
-    // const ddd = JSON.parse(fs.readFileSync('./data.json'))
-    for (let i = 0; i < sites.length; i++) {
-        const input = sites[i].url
+    const postDataPath = '../_data/postdata.json'
+    let postdata = {}
+    if (fs.existsSync(postDataPath)) {
+        postdata = JSON.parse(fs.readFileSync(postDataPath))
 
-        try {
-            const article = await extract(input)
-            if (article && article.content) {
-                const { document } = parseHTML(article.content)
-                document.querySelectorAll('a').forEach((a) => {
-                    const key = normaliseUrl(a.href)
-                    if (data[key]) {
-                        data[key].links.push(sites[i].url)
+        const sites = JSON.parse(fs.readFileSync('../_data/sites.json'))
+        const existingUrls = Object.values(postdata).map(s => normaliseUrl(s.url))
+
+        let currentSite = null
+        let currentUrl = null
+        for (let i = 0; i < sites.length; i++) {
+            currentSite = sites[i]
+            currentUrl = normaliseUrl(currentSite.url)
+            if (!existingUrls.includes(currentUrl)) {
+                console.log('Running for ' + currentSite.url)
+                postdata[currentUrl] = {
+                    ...currentSite,
+                    id: i + 1,
+                    urlKey: currentUrl,
+                    links: [],
+                    url: currentUrl,
+                }
+
+                try {
+                    const article = await extract(currentUrl)
+                    if (article && article.content) {
+                        const { document } = parseHTML(article.content)
+                        document.querySelectorAll('a').forEach((a) => {
+                            const foundLink = normaliseUrl(a.href)
+                            if (postdata[currentUrl]) {
+                                if (postdata[foundLink]) {
+                                    postdata[foundLink].links.push(currentUrl)
+                                }
+                            }
+                        })
                     }
-                })
+                } catch (err) {
+                    console.error(err)
+                }
             }
-        } catch (err) {
-            console.error(err)
+        }
+    } else {
+        postdata = generatePostDataFromSites()
+
+        let currentSite = null
+        let currentUrl = null
+        for (let i = 0; i < sites.length; i++) {
+            currentSite = sites[i]
+            currentUrl = normaliseUrl(currentSite.url)
+            console.log('Running for ' + currentUrl)
+            postdata[currentUrl] = {
+                ...currentSite,
+                id: i + 1,
+                urlKey: currentUrl,
+                links: [],
+                url: currentUrl,
+            }
+
+            try {
+                const article = await extract(currentUrl)
+                if (article && article.content) {
+                    const { document } = parseHTML(article.content)
+                    document.querySelectorAll('a').forEach((a) => {
+                        const foundLink = normaliseUrl(a.href)
+                        if (postdata[currentUrl]) {
+                            if (postdata[foundLink]) {
+                                postdata[foundLink].links.push(currentUrl)
+                            }
+                        }
+                    })
+                }
+            } catch (err) {
+                console.error(err)
+            }
         }
     }
+
+    fs.writeFileSync(postDataPath, JSON.stringify(postdata, null, 2))
 
     const nodes = []
     const edges = []
 
-    Object.values(data).forEach((d) => {
+    Object.values(postdata).forEach((d) => {
         const hosts = ['Andrew Canion', 'Jason Burk', 'Martin Feld']
         nodes.push({
             id: d.id,
@@ -69,10 +133,10 @@ const run = async () => {
             url: d.url,
             rss: d.rss,
             shape: 'box',
-            color: (d.id < 1000 || hosts.includes(d.name)) ? '#fdbd30' : '#03a4df',
+            color: (d.id >= 1000 || hosts.includes(d.name)) ? '#fdbd30' : '#03a4df',
         })
         d.links.forEach((link) => {
-            const id = data[link]?.id
+            const id = postdata[normaliseUrl(link)]?.id
             if (id && id !== d.id)
             {
                 edges.push({
@@ -80,10 +144,8 @@ const run = async () => {
                     to: id,
                 })
             }
-        }) 
+        })
     })
-
-    // fs.writeFileSync('../_data/data.json', JSON.stringify(data, '', 2))
 
     fs.writeFileSync('../_data/nodes.json', JSON.stringify({ nodes, edges}, '', 2))
 }
